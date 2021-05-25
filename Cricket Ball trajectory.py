a32a7f2579bfa081e5ec34d01eb6d503857498f9
Kailen Patel <kailenppatel@gmail.com>
2021-05-26 00:54:38 +0100

    Initial Test File Addition

---
+++ Cricket Ball trajectory.py
@@ -0,0 +1,53 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
+sns.set()
+
+def calc_trajectory(theta, v0, Cd = 0.6, den_air = 1.3, dt=0.1, n_iter = 10000):
+    g = 9.81
+    area_ball = 0.00403
+    mass = 0.16
+    #initialise lists
+    ax = np.zeros(n_iter)
+    ay = np.zeros(n_iter)
+    vx = np.zeros(n_iter)
+    vy = np.zeros(n_iter)
+    x = np.zeros(n_iter)
+    y = np.zeros(n_iter)
+    times = np.zeros(n_iter)
+
+    vx[0] = v0 * np.cos(theta * (2*np.pi)/360)
+    vy[0] = v0 * np.sin(theta * (2*np.pi)/360)
+
+    x[0] = 0
+    y[0] = 0
+    final_iter = 0
+    for i in range(n_iter -1):
+        dragy = 1/2 * Cd * den_air * area_ball * (vy[i])**2
+        dragx = 1/2 * Cd * den_air * area_ball * (vx[i])**2
+
+        accy = -(np.sign(vy[i])*dragy)/mass - g 
+        accx = -(np.sign(vx[i])*dragx)/mass
+
+        vy[i+1] = vy[i] + accy * dt
+        vx[i+1] = vx[i] + accx * dt
+
+        if y[i] < 0:
+            print('Ball hit the ground!')
+            print(f'Distance travelled {x[i]} with angle: {theta}')
+            final_iter = i
+            break
+        y[i+1] = y[i] + vy[i] *dt
+        x[i+1] = x[i] + vx[i] *dt
+
+    plt.plot(x[:final_iter] , y[:final_iter])
+
+for i in np.arange(0, 90, 5):
+    calc_trajectory(i, 31, dt=0.001, n_iter= 100000)
+plt.show()
+
+
+
+
+

ff5ab79b6dc966122fa35829a975fd3456234997
Kailen Patel <kailenppatel@gmail.com>
2021-05-26 01:21:55 +0100

    Add Angle Optimisation
    Plot optimum angle versus exit velocity to understand the relationship between them

--- Cricket Ball trajectory.py
+++ Cricket Ball trajectory.py
@@ -4,7 +4,7 @@ import seaborn as sns
 
 sns.set()
 
-def calc_trajectory(theta, v0, Cd = 0.6, den_air = 1.3, dt=0.1, n_iter = 10000):
+def calc_trajectory(theta, v0, Cd = 0.6, den_air = 1.3, dt=0.1, n_iter = 10000, plot= False):
     g = 9.81
     area_ball = 0.00403
     mass = 0.16
@@ -34,20 +34,31 @@ def calc_trajectory(theta, v0, Cd = 0.6, den_air = 1.3, dt=0.1, n_iter = 10000):
         vx[i+1] = vx[i] + accx * dt
 
         if y[i] < 0:
-            print('Ball hit the ground!')
-            print(f'Distance travelled {x[i]} with angle: {theta}')
             final_iter = i
             break
-        y[i+1] = y[i] + vy[i] *dt
-        x[i+1] = x[i] + vx[i] *dt
+        y[i+1] = y[i] + vy[i+1] *dt
+        x[i+1] = x[i] + vx[i+1] *dt
+    if plot == True:
+        plt.plot(x[:final_iter] , y[:final_iter])
+    return x[final_iter]
 
-    plt.plot(x[:final_iter] , y[:final_iter])
 
-for i in np.arange(0, 90, 5):
-    calc_trajectory(i, 31, dt=0.001, n_iter= 100000)
+
+best_angles = []
+velocities = np.arange(10, 60,1 )
+for v in velocities:
+    xvals = []
+    angles = []
+    for i in np.arange(0, 90, 0.1):
+        x_dist = calc_trajectory(i,v, dt=0.05, n_iter= 100000)
+        xvals.append(x_dist)
+        angles.append(i)
+    best_angles.append(angles[np.argmax(xvals)])
 plt.show()
 
-"this is a change"
-
+plt.plot(velocities, best_angles)
+plt.xlabel('bat exit velocity')
+plt.ylabel('optimum trajectory angle')
+    
 
 
